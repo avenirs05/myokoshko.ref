0b51a559267ef411e66b055a0dbd448b92e3bbc9
avenirs05 <avenirs05@gmail.com>
2018-03-23 01:04:07 +0200

    first

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Комплектующие для окон</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">     
+    
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/tiles.css" rel="stylesheet">
+   
+    <link href="/css/menu-mobile.css" rel="stylesheet">
+
+    <link href="/style.css" rel="stylesheet">
+    <link href="/css/moskit.css" rel="stylesheet">
+</head>
+
+<body> 
+
+    <div class="container-fluid d-none d-lg-block">
+        <div class="row">
+            <div class="col">
+                <a class="logo" href="/">
+                    <img class="logo__logo-img" src="/imgs/logo.png" width="90" alt="">
+                </a>
+                <div class="company-name">
+                    <div>Моё окошко</div>
+                    <div>Комплектующие для окон</div>
+                </div>
+            </div>
+            <div class="col">
+              
+            </div>
+        </div>
+    </div>
+
+    <script src="/js/jquery-3.1.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="/js/bootstrap.min.js"></script>
+
+    <!-- Tiles -->
+    <script src="/js/tiles.js"></script>
+
+    <!-- Menu mobile -->
+    <script src="/js/menu-mobile.js"></script>
+
+    <!-- My Own Js-scripts -->
+    <script src="/js/moskit-data.js"></script>
+    <script src="/js/func.js"></script>
+    <script src="/js/moskit-func.js"></script>       
+    <script src="/js/moskit-main.js"></script>
+    <script src="/js/main.js"></script>
+    <script src="/js/cart.js"></script>
+    <script src="/js/jquery.maskedinput.js" type="text/javascript"></script>
+
+</body> 
+</html>
+
+
+
